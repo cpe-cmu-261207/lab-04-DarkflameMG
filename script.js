e4d4a121f3485dab3c691d55ef65fc754baddd61
Bank <deknan70@gmail.com>
2021-07-23 23:26:10 +0700

    v1.2 add font style

--- script.js
+++ script.js
@@ -5,41 +5,42 @@ const listInput = (ev) => {
 }
 
 const mainDiv = document.createElement('div')
-mainDiv.setAttribute('class','max-w-sm mx-auto')
+mainDiv.setAttribute('class', 'max-w-sm mx-auto text-lg')
 const comDiv = document.createElement('div')
-comDiv.setAttribute('class','max-w-sm mx-auto p-2')
+comDiv.setAttribute('class', 'max-w-sm mx-auto p-2 text-lg')
 
 // Enter
 const input = document.getElementById("myinput")
 input.addEventListener('keyup', (ev) => {
-  if (ev.key === 'Enter') {
-   ev.preventDefault()
-   document.getElementById('myBtn').click()
-  }
+    if (ev.key === 'Enter') {
+        ev.preventDefault()
+        document.getElementById('myBtn').click()
+    }
 })
 
 // add list
 const addlist = () => {
     const Taskspan = document.createElement('p')
-    Taskspan.setAttribute('class','group flex justify-between p-2')
+    Taskspan.setAttribute('class', 'group flex justify-between p-2')
     const span = document.createElement('p')
     const btndiv = document.createElement('div')
+    btndiv.setAttribute('class', 'space-x-4')
     if (currentInput != "") {
         span.innerHTML = currentInput
         Taskspan.append(span)
-        
+
 
         const delBtn = document.createElement('button')
-        delBtn.setAttribute('class','text-white group-hover:bg-red-700 group-hover:text-black')
+        delBtn.setAttribute('class', 'text-white  group-hover:bg-red-400 group-hover:text-black')
         delBtn.innerHTML = "Delete"
         delBtn.addEventListener('click', () => {
             mainDiv.removeChild(Taskspan)
         })
 
         const comBtn = document.createElement('button')
-        comBtn.setAttribute('class','text-white group-hover:bg-green-700 group-hover:text-black')
+        comBtn.setAttribute('class', 'text-white  group-hover:bg-green-400 group-hover:text-black')
         comBtn.innerHTML = "Done"
-        comBtn.addEventListener('click',() => {
+        comBtn.addEventListener('click', () => {
             const del = document.createElement('del')
             //del.setAttribute('class','p-2')
             del.innerHTML = span.innerHTML
@@ -56,8 +57,7 @@ const addlist = () => {
         document.body.append(mainDiv)
         document.body.append(comDiv)
     }
-    else
-    {
+    else {
         alert("Task cannot be empty")
     }
 }
